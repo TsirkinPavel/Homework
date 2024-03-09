26cc2639114017eece7327b85db4fec9f89c3da8
Tsirkin Pavel <master878@yandex.ru>
2024-03-09 22:41:14 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,39 @@
-﻿//Примеры: [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”] 
-//[“1234”, “1567”, “-2”, “computer science”] → [“-2”] 
-//[“Russia”, “Denmark”, “Kazan”] → []
+﻿
+int CountArraySize(string[] Array1)
+{
+    int size = 0;
+    foreach (string element in Array1)
+        if (element.Length <= 3) size++;
+    return size;
+}
 
+string[] MakeNewArray(string[] Array1, int arraySize)
+{
+    int i = 0;
+    string[] newArray = new string[arraySize];
+    foreach (string element in Array1)
+    {
+        if (element.Length <= 3)
+        {
+            newArray[i] = element;
+            i++;
+        }
+    }
+    return newArray;
+}
+
+void PrString(string[] chars)
+{
+    if (chars.Length == 0)
+        System.Console.Write("[]");
+
+    else
+    {
+        char sim = '"';
+        Console.WriteLine($"[{sim}{string.Join($"{sim}, {sim}", chars)}{sim}]");
+    }
+}
+string[] Array1 = { "rkg", "RFJTF", ";-)", "123" };
+int arraySize = CountArraySize(Array1);
+string[] newArray = MakeNewArray(Array1, arraySize);
+PrString(newArray);
